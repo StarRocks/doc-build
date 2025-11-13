a284a3ff39869d5f5827e3835e303b5ab68d03de
DanRoscigno <dan@roscigno.com>
2025-11-13 17:23:54 -0500

    add status log
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- checksettings/find_invisible.sh
+++ checksettings/find_invisible.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo "Creating temp dir and getting visible parameters from StarRocks DB..."
 mkdir -p temp
 mysql -P9030 -h 127.0.0.1 -u root < ./helpers/visible-script > ./temp/visible-variables.txt
+echo "Getting invisible parameters from StarRocks DB..."
 mysql -P9030 -h 127.0.0.1 -u root < ./helpers/all-script > ./temp/all-variables.txt
 /opt/homebrew/opt/grep/libexec/gnubin/grep -vf ./temp/visible-variables.txt ./temp/all-variables.txt > ./temp/invisible
+echo "Checking the docs for invisible parameters..."
 {
     read
     while IFS=$'\t' read -r first_field rest_of_line; do
@@ -18,5 +21,6 @@ mysql -P9030 -h 127.0.0.1 -u root < ./helpers/all-script > ./temp/all-variables.
         #grep "\#\#\# $first_field" /Users/droscign/GitHub/starrocks/docs/en/sql-reference/System_variable.md| grep -v ":0"
         # Add your action here using $first_field
     done
-} < ./temp/invisible >./results
+} < ./temp/invisible > ./results
 
+echo "Examine the file ./results"
