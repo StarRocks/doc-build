6886321cf2458c292d4e7eca2a0f1e3f016d3c55
DanRoscigno <dan@roscigno.com>
2026-04-16 09:43:28 -0400

    trace
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@ yarn install --frozen-lockfile
 git clone git@github.com:StarRocks/starrocks.git temp
 npm run copy
 export DOCUSAURUS_IGNORE_SSG_WARNINGS=true
-export BUILD_FAST=true
-export NODE_OPTIONS="--max-old-space-size=12192"
+export NODE_OPTIONS="--max-old-space-size=12288 --trace-gc"
 yarn clear && yarn build && yarn serve
