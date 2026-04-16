8706f4635781c07ecd17a45d83aae9cf32307c83
Dan Roscigno <dan@roscigno.com>
2026-04-16 11:09:14 -0400

    Apply suggestion from @DanRoscigno
    turn off persistent cache

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,19 @@ const config = {
 
   future: {
     v4: true,
-    experimental_faster: true,
+    experimental_faster:
+            {
+            // Verbose object: easier to independently test single attributes
+            swcJsLoader: true,
+            swcJsMinimizer: true,
+            swcHtmlMinimizer: true,
+            lightningCssMinimizer: true,
+            mdxCrossCompilerCache: true,
+            rspackBundler: true,
+            rspackPersistentCache: false,
+            ssgWorkerThreads: true,
+            gitEagerVcs: true,
+          },
   },
 
   i18n: {
